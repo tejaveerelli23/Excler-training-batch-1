5c31a392b76759d0d4bcf9c828100ed2bd8598f8
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 10:28:59 +0530

    greater number

---
+++ Demo3.java
@@ -0,0 +1,12 @@
+public class Demo3 {
+    public static void main(String[] main) {
+        int a = 20;
+        int b = 10;
+        if (a > b) {
+            System.out.println("A is greater than B");
+        } else {
+            System.out.println("A is less than or equal to B");
+        }
+    }
+}
+
