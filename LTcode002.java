969779bd10f3a8845caa0f766af0764a5503d99c
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:34:49 +0530

    reversing

---
+++ LTcode002.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class LTcode002 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n,r,rev=0;
+    System.out.println("Enter Values:");
+    n = sc.nextInt();
+    System.out.println("Before Reversing:"+n);
+    while(n!=0){
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    
+    System.out.println("After Reversing:"+rev);
+}
+}
