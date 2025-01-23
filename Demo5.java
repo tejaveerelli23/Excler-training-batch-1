6554fe49d4808235e8f4479062add1dcd0969c00
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:21:14 +0530

    100 numbers

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 100; i++) {
+            if(i==98)
+            continue; 
+            System.out.println(i);
+        }
+    }
+}
+
