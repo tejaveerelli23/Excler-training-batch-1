5497ff9f5603161578bc60cec3aa655290f92a08
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:27:41 +0530

    fizzbizz

---
+++ Demo029.java
@@ -0,0 +1,10 @@
+
+
+public class Demo029 {
+    public static void main(String[] args) {
+      String[] frnds={"sai","teja"};
+      for(String friend:frnds){
+        System.out.println(friend);
+      }
+          
+      }}
