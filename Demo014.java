7abe87ee8cddbb318787d0e5aa68aa20adb17c23
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:24:54 +0530

    swapping

---
+++ Demo014.java
@@ -0,0 +1,23 @@
+public class Demo014 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Initial value of a: " + a);
+
+        
+        System.out.println("Post-increment (a++): " + a++); 
+        System.out.println("Value of a after post-increment: " + a);
+
+        
+        System.out.println("Pre-increment (++a): " + ++a);
+        System.out.println("Value of a after pre-increment: " + a);
+
+       
+        System.out.println("Post-decrement (a--): " + a--); 
+        System.out.println("Value of a after post-decrement: " + a);
+
+       
+        System.out.println("Pre-decrement (--a): " + --a);
+        System.out.println("Value of a after pre-decrement: " + a);
+    }
+}
