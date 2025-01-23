0f143da75eb4211ce65b939d1c98f0d17eace85d
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:28:41 +0530

    array files

---
+++ Demo030.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class Demo030 {
+
+    static class ArrayListCode {
+        void arrayListMethod() {
+            ArrayList<Integer> arrayList1 = new ArrayList<>();  
+            arrayList1.add(100);
+            arrayList1.add(200);
+            arrayList1.add(100);
+            arrayList1.add(0);
+            arrayList1.add(900);
+            arrayList1.add(600);
+
+            System.out.println("Elements of the list: ");
+            
+            Iterator<Integer> iterate = arrayList1.iterator();
+            
+            while (iterate.hasNext()) {
+                System.out.println(iterate.next());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayListCode obj = new ArrayListCode();
+        obj.arrayListMethod();
+    }
+}
