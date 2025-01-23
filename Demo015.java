f30c400f005bb536b4fb9372874cd1e8d52331a9
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 16:25:42 +0530

    swapping

---
+++ Demo015.java
@@ -0,0 +1,17 @@
+public class Demo015 {
+        public static void main(String[] args) {
+            int a = 5, b = 10;
+    
+            System.out.println("Before swapping:");
+            System.out.println("a = " + a + ", b = " + b);
+    
+            
+            int temp = a;
+            a = b;
+            b = temp;
+    
+            System.out.println("After swapping:");
+            System.out.println("a = " + a + ", b = " + b);
+        }
+    }
+
