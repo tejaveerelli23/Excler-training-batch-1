316afdadd3959fc4edc3e1f7f203bc3aa9791251
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-23 10:28:07 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,11 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        String name = "teja";
+        String pwd = "teja@143";
+        while (name.equals("teja") && pwd.equals("teja@143")) {
+            // Corrected println syntax
+            System.out.println("hello teja Welcome to Mallareddy University");
+            break;  
+        }
+    }
+}
