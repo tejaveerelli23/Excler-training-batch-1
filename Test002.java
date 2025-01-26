b83eeb9c379b6a76086706aebb3749d83b3c6ca8
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-26 20:48:41 +0530

    Happy sankranthi

---
+++ Test002.java
@@ -0,0 +1,14 @@
+
+   import java.util.*;
+class Test002{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter reply: ");
+        String reply = sc.next();
+        while(reply.equals("yes")||reply.equals("Yes")||reply.equals("YES")){
+            System.out.println("Happy sankranthi.");
+            System.out.print("Enter reply: ");
+            reply = sc.next();
+        }
+    }
+}
