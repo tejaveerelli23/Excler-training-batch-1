91e79bf900226259bbc40bea185438cfeb691dda
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-27 09:55:55 +0530

    lowercase or uppercase or digit

---
+++ Demo039.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class Demo039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char character = sc.next().charAt(0);
+        if (Character.isUpperCase(character)) {
+            System.out.println("The character " + character + " is an uppercase letter.");
+        } else if (Character.isLowerCase(character)) {
+            System.out.println("The character " + character + " is a lowercase letter.");
+        } else if(Character.isDigit(character)) {
+            System.out.println("The character " + character + " is a digit.");
+        }else{
+            System.out.println("Enter a valid character.");
+        }
+    }
+}
+
