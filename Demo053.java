5ee974a56d4a5e3c3050dfd0f9b61b535a7b5072
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-29 09:50:40 +0530

    number is strong or not.

---
+++ Demo053.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Demo053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int originalNumber = number, sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum += factorial(digit);
+            number /= 10;
+        }
+
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is a Strong number.");
+        } else {
+            System.out.println(originalNumber + " is not a Strong number.");
+        }
+
+        scanner.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return 
+        fact;
+    }
+}
