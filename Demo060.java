6ccb0ad9e7da9976d3245f624542611d64bca705
tejaveerelli23 <verellitejateja@gmail.com>
2025-02-05 09:51:01 +0530

    Demo060.java

--- Demo060.java
+++ Demo060.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
-class demo060 {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        int num=scanner.nextInt();
-        int a=0,b=1,temp;
-        System.out.println("Fibonacci series: ");
-        for(int i=1;i<num;i++){
-            System.out.print(a + " ");
-                temp = a + b;
-                a = b;
-                b = temp;
+public class Demo060{
+    public static void main(String[] args){
+        int a = 20;
+        Fibo obj = new Fibo();
+        obj.fib(a);
+    }
+}
+class Fibo{
+    void fib(int num){
+        int a = 0, b = 1, c;
+        System.out.print(a + " " + b);
+        for(int i = 2; i < num; i++){
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
         }
     }
 }
+    
